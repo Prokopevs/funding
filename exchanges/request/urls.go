d17a499fd3c66b6658532c412d783049a081965b
Prokopevs <prokopev_savva@mail.ru>
2024-02-01 20:13:46 +0300

    url fix

--- exchanges/request/urls.go
+++ exchanges/request/urls.go
@@ -7,7 +7,7 @@ func getUrls() []string {
 		"https://fapi.binance.com/fapi/v1/premiumIndex",
 		"https://contract.mexc.com/api/v1/contract/ticker",
 		"https://api.gateio.ws/api/v4/futures/usdt/tickers",
-		"https://api-cloud.bitmart.com/contract/public/details",
+		"https://api.coinex.com/perpetual/v1/market/ticker/all",
 		"https://open-api.bingx.com/openApi/swap/v2/quote/premiumIndex",
 		"https://api-futures.kucoin.com/api/v1/contracts/active",
 		"https://api-cloud.bitmart.com/contract/public/details",
